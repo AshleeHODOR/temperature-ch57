5e3cbdda486a83354b221f48920443997e7d2bd6
AshleeHODOR <hodor1987@yahoo.com>
2025-03-29 14:43:10 -0700

    firstclassnotes

---
+++ variables.js
@@ -0,0 +1,41 @@
+console.log("Variables and constants");
+//strings
+let myName = "Ashlee";
+let lastName = "HODOR"; 
+//numbers
+var myAGE = 12;
+//boolean (true or false)
+let isStudent = false; 
+
+//display the values
+console.log("Ashlee"); 
+console.log("HODOR")
+
+//concatenation
+console.log("My name is " + "Ashlee" + "HODOR"); 
+
+//operations
+
+let num1=10;
+let num2=20;
+
+let addition = num1 + num2; 
+let substraction = num1 - num2; 
+let multiplication = num1 * num2; 
+let division = num1 / num2; 
+
+console.log(addition); 
+console.log(substraction);
+console.log(multiplication);
+console.log(division); 
+
+//challenge 1
+
+let a;
+const pi = 3.1416; //can't change 
+let r=3; 
+
+a = pi * (r*r);
+
+console.log("The result is: " + a); 
+
