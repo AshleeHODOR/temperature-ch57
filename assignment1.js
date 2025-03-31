ecd4c65a7396663de26f91530efc6d513ea9393b
AshleeHODOR <hodor1987@yahoo.com>
2025-03-30 17:07:42 -0700

    class1homeowrk

---
+++ assignment1.js
@@ -0,0 +1,35 @@
+//Declare Variables
+//using let
+let character = "Sid Manny and Diego";
+let action = "being onstage";
+let object = "a shiny rock";
+let place = "The Eras Tour";
+let time = "10,000 years later";
+
+//using "const
+const occasion = "The Ice Age"; 
+
+//create the story
+console.log("---Funny Story---"); 
+console.log("occassion" + has "character" seeing + "object"); 
+console.log("Sid Manny and Diego" + with "shiny rock" + grab each other);
+console.log("10,000 years later" + while at the + "The Eras Tour");
+console.log("stage" + "The Era Tour" + singing Fearless + "Sid Manny and Diego");
+console.log("Taylor Swift" + "The Eras Tour" + singing Fearless + "sees Sid, Manny, and Diego");
+console.log("Taylor Swift" in shock + hugs + "Sid" + "Manny" + "Diego");
+console.log(all join in + and finish + singing Fearless); 
+
+
+//change values to create different story
+let character = "Taylor Swift";
+let action = "goes back in time 10,000 years ago";
+let object = "holding mic";
+let place = "The stage at the Eras Tour";
+let time = "the year 2024"; 
+
+console.log("Taylor Swift" + while at "The Era Tour" + "Singing Fearless");
+console.log(is taken back + "Sid" + "Manny" + "Diego");
+console.log("10,000 years later" + back in the present + "Taylor Swift" rocks out with "Sid" + "Diego" + "Manny");
+console.log(Everyone rocks out + starts "Singing Fearless");
+
+//Note: "const" variables cannot be reassigned, so keep that in mind while experimenting
