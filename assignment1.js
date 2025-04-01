717ac0db98429048de7da499053b6219b464e176
AshleeHODOR <hodor1987@yahoo.com>
2025-03-31 17:04:29 -0700

    homeworkclass1

--- assignment1.js
+++ assignment1.js
@@ -1,7 +1,7 @@
 //Declare Variables
 //using let
 let character = "Sid Manny and Diego";
-let action = "being onstage";
+let action = "onstage";
 let object = "a shiny rock";
 let place = "The Eras Tour";
 let time = "10,000 years later";
@@ -11,25 +11,25 @@ const occasion = "The Ice Age";
 
 //create the story
 console.log("---Funny Story---"); 
-console.log("occassion" + has "character" seeing + "object"); 
-console.log("Sid Manny and Diego" + with "shiny rock" + grab each other);
-console.log("10,000 years later" + while at the + "The Eras Tour");
-console.log("stage" + "The Era Tour" + singing Fearless + "Sid Manny and Diego");
-console.log("Taylor Swift" + "The Eras Tour" + singing Fearless + "sees Sid, Manny, and Diego");
-console.log("Taylor Swift" in shock + hugs + "Sid" + "Manny" + "Diego");
-console.log(all join in + and finish + singing Fearless); 
+console.log("occassion" + "has character seeing" + "object"); 
+console.log("Sid Manny and Diego" + "with shiny rock");
+console.log("10,000 years later" + "while at the" + place);
+console.log("stage" + "The Era Tour" + "singing Fearless" + "Sid Manny and Diego");
+console.log(character + place + "singing Fearless + character);
+console.log("Taylor Swift" + "hugs" + "Sid" + "Manny" + "Diego");
+console.log(place + action + "singing Fearless"); 
 
 
 //change values to create different story
-let character = "Taylor Swift";
-let action = "goes back in time 10,000 years ago";
-let object = "holding mic";
-let place = "The stage at the Eras Tour";
-let time = "the year 2024"; 
+character = "Taylor Swift";
+action = "goes back in time 10,000 years ago";
+object = "holding mic";
+place = "The stage at the Eras Tour";
+time = "the year 2024"; 
 
-console.log("Taylor Swift" + while at "The Era Tour" + "Singing Fearless");
-console.log(is taken back + "Sid" + "Manny" + "Diego");
-console.log("10,000 years later" + back in the present + "Taylor Swift" rocks out with "Sid" + "Diego" + "Manny");
-console.log(Everyone rocks out + starts "Singing Fearless");
+console.log("Taylor Swift" + place + "Singing Fearless");
+console.log(action + character);
+console.log(time + + charcter + "rocks out with" + "Sid" + "Diego" + "Manny");
+console.log(place + "starts singing Fearless");
 
 //Note: "const" variables cannot be reassigned, so keep that in mind while experimenting
