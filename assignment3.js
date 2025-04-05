92b00d93d687a8e30035e1c67a0d776493c93a90
AshleeHODOR <hodor1987@yahoo.com>
2025-04-05 10:13:27 -0700

    class3homework

--- assignment3.js
+++ assignment3.js
@@ -1,18 +1,21 @@
 //Convert Temperature
 
 function ConvertTemperature(){
-    let usertemp = prompt("Enter your temperature");
+    let temp = prompt("Enter the temperature");
     let C = Celsius("Enter C");
     let F = Fahrenheit("Enter F");
     document.getElementById("results").innerHTML=`$usertemp`;
 }
 
-if C{("Fahrenheit = (Celsius * 9/5");
-    console.log("Show usertemp in C");
+if (Fahrenheit = Celsius * 9/5){
+    console.log("Show usertemp in F");
 }
-else if{("Celsius = (Fahrenheit-32");
-    console.log("show usertemp in F");
+else if(Celsius = Fahrenheit-32){
+    console.log("show usertemp in C");
+
+    p.innerHTML="Your results are: " + usertemp; 
 }
+
 
 
 //Bonus
